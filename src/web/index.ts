f5f25ded735556d4a2bba76059bcbc4081aa72c2
“chrisshank” <chris.shank.23@gmail.com>
2026-07-02 12:00:30 -0700

    remove import

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,3 @@
-// export { attr, observedAttributesOf, syncAttrSignal } from "./attr";
 export { Diagram, type Padding } from "./diagram";
 export { MdMarker } from "./md-marker";
 export { MdTex } from "./md-tex";
